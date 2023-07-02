8ff2baf0671943fe0d5288f4d8c4c367e1b41e89
Faraz Khoubsirat <faraz@Faraz-Khoubsirat.local>
2023-07-02 12:26:27 -0400

    update

--- embed.py
+++ embed.py
@@ -32,7 +32,7 @@ def reduce(embeds):
 def gen_summaries(texts):
     prompt = f"""
     # Task
-    Given the list of items below, summarize the list of items in 3-4 bullet points.
+    Given the list of items below, summarize the list of items in 3-4 bullet points, return only the bullet points.
 
     # List of items
 
@@ -49,6 +49,42 @@ def gen_summaries(texts):
         )
         summaries.append(res.choices[0].message.content)
     return summaries
+
+@st.cache_data
+def gen_keywords(texts):
+    prompt = f"""
+    # Task
+    Given the list of items below, generate a comma seperated list of 3 keywords that best describe the list of items. ONLY RETURN THE KEYWORDS
+
+    # Example List of items
+    1. I love eating fruits in the morning
+    2. Mornings, i start my day by eating two fruits at least
+    3. fruits in the morning is the best way to start the day
+
+    # Example Keywords
+    fruits, morning, day
+
+    # List of items
+
+    """
+    output = f"""
+
+    # Example Keywords
+
+    """
+    keywords = []
+    for text in texts:
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt+text+output},
+            ],
+            max_tokens=500
+        )
+        keywords.append(res.choices[0].message.content)
+    return keywords
+
 
 st.title("""Kaggle Tips and Tricks""")
 
@@ -73,7 +109,7 @@ with st.spinner("Reducing dimensionality..."):
     tips_tricks_df['y'] = umap_embeds[:,1]
 
 # now cluster using k-means
-k = st.slider("Number of clusters", 2, 10, 8)
+k = st.slider("Number of clusters", 2, 10, 6)
 from sklearn.cluster import KMeans
 with st.spinner("Clustering tips..."):
     kmeans = KMeans(n_clusters=k, random_state=0).fit(umap_embeds)
@@ -86,9 +122,12 @@ tips_tricks_df['trick'] = tips_tricks_df['trick'].apply(lambda x: "<br>".join(wr
 with st.spinner("Generating summaries..."):
     cluster_groups = tips_tricks_df.groupby('cluster').agg({'trick': lambda x: "\n".join(x)}).reset_index()
     cluster_groups['summary'] = gen_summaries(cluster_groups['trick'].tolist())
+    cluster_groups['keywords'] = gen_keywords(cluster_groups['trick'].tolist())
     tips_tricks_df['trick_cluster_summary'] = cluster_groups['summary'][tips_tricks_df['cluster']].values
+    tips_tricks_df['trick_cluster_keywords'] = cluster_groups['keywords'][tips_tricks_df['cluster']].values
 
-tips_tricks_df['color'] = tips_tricks_df['cluster'].astype(str)
+
+tips_tricks_df['color'] = tips_tricks_df['trick_cluster_keywords'].astype(str)
 c_fig = px.scatter(tips_tricks_df, x="x", y="y", color="color",
                    hover_data={
                           'x': False,
@@ -104,5 +143,5 @@ st.plotly_chart(c_fig, use_container_width=True, theme="streamlit")
 
 st.write("## Summaries")
 for i, row in cluster_groups.iterrows():
-    with st.expander("Cluster "+str(row['cluster'])):
+    with st.expander(row['keywords']):
         st.markdown(row['summary'])
